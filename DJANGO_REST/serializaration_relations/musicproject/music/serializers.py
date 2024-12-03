6a645fb856ce13109c07f9285ac7b9745cb64d9f
ashishsingh2b <ashishsingh2b@gmail.com>
2024-12-03 15:27:10 +0530

    3 rd december practice

--- DJANGO_REST/serializaration_relations/musicproject/music/serializers.py
+++ DJANGO_REST/serializaration_relations/musicproject/music/serializers.py
@@ -4,21 +4,19 @@ from .models import Singer, Song
 
 
 class SongSerializer(serializers.ModelSerializer):
-    # singer = SingerSerializer(read_only=True)  # Nested singer serializer
-    # singer = serializers.StringRelatedField(read_only=True)  # Nested singer serializer
-    
-    
-
     class Meta:
         model = Song
-        fields = ['id', 'title', 'release_date', 'singer']
+        fields = ['id', 'title', 'release_date']
 
 
 class SingerSerializer(serializers.ModelSerializer):
-    songs = serializers.CharField(source='songs.name')
+    #songs = serializers.CharField(source='songs.name')
 
-   # Songs = serializers.PrimaryKeyRelatedField(read_only=True)  # Nested singer serializer
+    #songs = serializers.PrimaryKeyRelatedField(many=True,read_only=True)  # Nested singer serializer
     #songs = serializers.StringRelatedField(many=True,read_only=True)  # Nested singer serializer
+    #songs = serializers.HyperlinkedRelatedField(many=True,read_only=True,view_name='song-detail')  # Nested singer serializer
+    #songs = serializers.SlugRelatedField(many=True,read_only=True,slug_field='title')  # Nested singer serializer
+    songs = serializers.HyperlinkedIdentityField(view_name='song-detail')  # Nested singer serializer
     class Meta:
         model = Singer
         fields = ['id', 'name', 'birth_date','songs']
